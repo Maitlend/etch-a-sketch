fcaed66980b50a79bef7e321471be147bb00d0b4
Maitlend <Maitlend.merrill@gmail.com>
2022-10-28 17:18:39 -0400

    Add grid size user input button and implement functionality

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const container = document.getElementById("container");
+const gridSizeBtn = document.getElementById("grid-size-btn");
 
-for(let i = 0; i < 256; i++){
-  const div = document.createElement('div');
-  div.classList.add('grid-item'); 
-  container.appendChild(div);
+gridSizeBtn.addEventListener("click", gridSizeHandler);
+
+buildGrid(16);
+
+function gridSizeHandler(){
+  const size = prompt("Please enter a size for grid row (ex. 64): ");
+  removeGrid();
+  buildGrid(size);
+}
+
+function buildGrid(gridSize){
+  for(let i = 0; i < gridSize*gridSize; i++){
+    const div = document.createElement('div');
+    div.classList.add('grid-item');
+    div.addEventListener("mouseover", colorCell);
+    container.appendChild(div);
+  }
+  container.setAttribute("style", `grid-template-columns: repeat(${gridSize}, 1fr);`);
+}
+
+function colorCell(){
+  this.classList.add('highlighted');
+}
+
+function removeGrid(){
+  while(container.firstChild){
+    container.removeChild(container.firstChild);
+  }
 }
